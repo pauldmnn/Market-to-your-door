6e2028d4527dfa8a3b591c38da28a8e182865474
pauldmnn <pauldmnn@gmail.com>
2025-03-06 14:30:42 +0000

    Added views and template for sorting and filtering

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Product, Category
 
 def product_list(request):
     """
-    A view to display all products including searching and sorting
+    A view to display all products including searching, sorting and filtering
     """
 
     query = request.GET.get('q', '')
@@ -15,8 +15,34 @@ def product_list(request):
     else:
         products = Product.objects.all()
 
+        #Filtering 
+        name_filter = request.GET.get('name', '')
+        category_filter = request.GET.get('category', '')
+        min_price = request.GET.get('min_price', '')
+        max_price = request.GET.get('max_price', '')
+
+        if name_filter:
+            products = products.filter(name__icontains=name_filter)
+        if category_filter:
+            products = products.filter(category__slug=category_filter)
+        if min_price:
+            products = products.filter(price__gte=min_price)
+        if max_price:
+            products = products.filter(price__gte=max_price)
+
+        sort_option = request.GET.get('sort', 'name')
+        if sort_option in ['name', '-name', 'price', '-price', ]:
+            products = products.order_by(sort_option)
+
     return render(request, 'products/products.html', {
-        'products': products, 'query': query
+        'products': products, 
+        'query': query,
+        'name_filter': name_filter,
+        'category_filter': category_filter,
+        'min_price': min_price,
+        'max_price': max_price,
+        'sort_option': sort_option,
+        'categories': Category.objects.all(),
     })
 
 
