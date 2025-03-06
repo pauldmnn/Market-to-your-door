3beb34841ff79cb5b445207f2889f69c568b2c91
pauldmnn <pauldmnn@gmail.com>
2025-03-06 11:44:28 +0000

    Added views and template for category

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import product_list, product_detail
+from . import views
 
 urlpatterns = [
     path('', product_list, name='product_list'),
-    path('products/<slug:slug>', product_detail, name='product_detail')
+    path('products/<slug:slug>', product_detail, name='product_detail'),
+    path('market/<slug:category_slug>/', views.category_products, name='category_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -8,9 +10,20 @@ def product_list(request):
     """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET('q')
+            if not query:
+                messages.error(request, "No items found. Please try again!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     return render(request, 'products/products.html', {
-        'products': products,
+        'products': products, 'search_term': query
     })
 
 
@@ -24,3 +37,16 @@ def product_detail(request, slug):
     return render(request, 'products/product_detail.html', {
         'product': product,
     })
+
+
+def category_products(request, category_slug):
+    """
+    A view to retrieve the categories
+    """
+    category = get_object_or_404(Category, slug=category_slug)
+
+    products = Product.objects.filter(category=category)
+    return render(request, 'products/category_products.html', {
+        'category': category,
+        'products': products
+    } )
