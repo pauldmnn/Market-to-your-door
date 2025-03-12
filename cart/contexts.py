9c11b39019cd9eabcec01b58382c4374fbf9d4fb
pauldmnn <pauldmnn@gmail.com>
2025-03-12 12:40:01 +0000

    Added cart total context

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from products.models import Product
+from decimal import Decimal
 
 
 def cart(request):
@@ -13,13 +14,13 @@ def cart_total(request):
     Context processor to make the grand total available in all templates.
     """
     cart = request.session.get('cart', {})
-    overall_total = 0.00  
+    overall_total = Decimal('0.00')  
 
     for slug, item in cart.items():
         try:
             product = Product.objects.get(slug=slug)
-            overall_total += product.price * item['quantity']
+            overall_total +=  Decimal(product.price) * Decimal(item['quantity'])
         except Product.DoesNotExist:
             pass  
 
-    return {'overall_total': overall_total}
+    return {'overall_total': overall_total.quantize(Decimal('0.01'))}
