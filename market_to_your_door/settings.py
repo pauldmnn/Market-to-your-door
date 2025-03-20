30030fcb6c23a2c343c5f1b5621b8cf0a76c8aef
pauldmnn <pauldmnn@gmail.com>
2025-03-20 11:27:56 +0000

    Added postgesql database and migarted

--- market_to_your_door/settings.py
+++ market_to_your_door/settings.py
@@ -11,15 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
 import os
 
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,11 +121,16 @@ WSGI_APPLICATION = 'market_to_your_door.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
