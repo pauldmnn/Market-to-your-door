32a9379940042d1ba90efed4ed6a937b861076bd
pauldmnn <pauldmnn@gmail.com>
2025-03-24 14:06:58 +0000

    Added matching style to the rest of the site for the registration page

--- market/views.py
+++ market/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from allauth.account.forms import SignupForm
+
 
 
 def index(request):
@@ -6,3 +8,10 @@ def index(request):
     A view to return the index page
     """
     return render(request, 'market/index.html')
+
+
+
+def home(request):
+    return render(request, "market.html", {
+        "form": SignupForm(),
+    })
