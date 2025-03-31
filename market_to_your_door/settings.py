2ec08b678fc58b18957f1f3f95f96570f6fb08cb
pauldmnn <pauldmnn@gmail.com>
2025-03-31 21:34:06 +0100

    Added real email functionality

--- market_to_your_door/settings.py
+++ market_to_your_door/settings.py
@@ -229,7 +229,22 @@ if not STRIPE_PUBLIC_KEY or not STRIPE_SECRET_KEY:
     raise ValueError("Stripe API keys are missing from the environment variables!")
 if not STRIPE_WEBHOOK_SECRET:
     raise ValueError("Stripe webhook secret is missing from the environment variables!")
-DEFAULT_FROM_EMAIL = 'markettyd@example.com'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'markettyd@example.com'
+
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
