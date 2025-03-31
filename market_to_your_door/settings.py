8cfacffce6e2475885977d51ea63fc135d33f7b7
pauldmnn <pauldmnn@gmail.com>
2025-03-31 18:07:34 +0100

    Fixing static error

--- market_to_your_door/settings.py
+++ market_to_your_door/settings.py
@@ -195,7 +195,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'market-to-your-door'
+    AWS_STORAGE_BUCKET_NAME = 'markettyd'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
