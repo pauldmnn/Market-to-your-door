d0bae119d7751342e4cdc98d1323d93752c47171
pauldmnn <pauldmnn@gmail.com>
2025-04-01 10:52:21 +0100

    Fixed typo error in admin files

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.registe(UserProfile)
+admin.site.register(UserProfile)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Review
 
-admin.site.registe(Review)
+admin.site.register(Review)
